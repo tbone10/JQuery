e4b5625b71b716a53927fbda8ac84103a48a2e64
Timothy Morgan <tmorgan1010@outlook.com>
2015-11-09 19:33:06 -0500

    Initial JQuery Commit

---
+++ listings/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajax( {
+    	url:"https://api.myjson.com/bins/2sadq?pretty=1", 
+    	dataType: "json",
+    	success: function(response) {
+    		$.each(response.apartments, function(i, apartment) {
+    			var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+				var listing = "<a href='#' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>";
+				   	$(".apartments").append(listing);
+    		});
+    	},
+    	error: function(error) {
+      			 console.log(error);
+      			}
+    });
+  		 $(".filter").click(function() {
+		   $(".filter").removeClass("active");
+		   $(this).addClass("active");
+			
+			$(".listings").show();
+
+		var city = $(this).attr("id");
+		if(city != "all") {
+    		$(".listings").not("." + city).css("display","none");
+    	}
+ });   
+});
+
+
--- project-3/index.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    // YOUR CODE HERE!
-});
