4b775e3300439cebc1fac2dfe4bc17c753eeb11b
tbone10 <tmorgan1010@outlook.com>
2015-12-06 18:35:07 -0500

    Modified FAQ/index.html and .js and style.css

--- faq/index.js
+++ faq/index.js
@@ -3,21 +3,26 @@ $(".question").click(function() {
  	$(this).next().fadeToggle("fast");
 	$(this).children().toggleClass("collapse");
 })
-//  $("#a1").fadeToggle("fast");
-//  $("#arrow1-down, #arrow1-up").toggleClass("collapse");
-// })
+ //  $("#a1").fadeToggle("fast");
+ //  $("#arrow1-down, #arrow1-up").toggleClass("collapse");
+ // })
 
-// $("#q2").click(function() {
-//  $("#a2").fadeToggle("fast");
-//  $("#arrow2-down, #arrow2-up").toggleClass("collapse");
-// })
+ // $("#q2").click(function() {
+ //  $("#a2").fadeToggle("fast");
+ //  $("#arrow2-down, #arrow2-up").toggleClass("collapse");
+ // })
 
-// $("#q3").click(function() {
-//  $("#a3").fadeToggle("fast");
-//  $("#arrow3-down, #arrow3-up").toggleClass("collapse");
-// })
+ // $("#q3").click(function() {
+ //  $("#a3").fadeToggle("fast");
+ //  $("#arrow3-down, #arrow3-up").toggleClass("collapse");
+ // })
 
-// $("#q4").click(function() {
-//  $("#a4").fadeToggle("fast");
-//  $("#arrow4-down, #arrow4-up").toggleClass("collapse");
-// })
+ // $("#q4").click(function() {
+ //  $("#a4").fadeToggle("fast");
+ //  $("#arrow4-down, #arrow4-up").toggleClass("collapse");
+ // })
+
+ // $("#q5").click(function() {
+ //  $("#a5").fadeToggle("fast");
+ //  $("#arrow5-down, #arrow5-up").toggleClass("collapse");
+ // })
